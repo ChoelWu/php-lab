6398e9d7e6a851b178a9e42824828f3deb5a5c31
ChoelWu <choel_wu@foxmail.com>
2019-01-08 23:13:23 +0800

    case20190108

---
+++ case20190108/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: choel
+ * Date: 19-1-8
+ * Time: 下午10:29
+ */
+
+class Student
+{
+    private $name = 'choel';
+    public $age = 24;
+
+    public function __get($name)
+    {
+        return $this->name;
+    }
+    public function __set($name, $value)
+    {
+        $this->name = $value;
+//        return $this->name = 'wuchao';
+    }
+}
+
+$student = new Student();
+$student->name = 'wuchao';
+echo $student->name;
+//echo $student->name;
+
+
+// 外部访问静态元素可以使用 :: ClassName::static
+// 外部对象可以访问静态方法，不能访问静态属性
